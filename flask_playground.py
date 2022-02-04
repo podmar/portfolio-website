c1e8b49cdc8568cc6b8a6dea06fcd84eefec1572
podmar <marta.podziewska@gmail.com>
2022-02-04 18:23:29 +0100

    working page structure

--- flask_playground.py
+++ flask_playground.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home(): 
-    return render_template("page.html")
+    return render_template("home.html")
+
+@app.route('/projects/')
+def projects(): 
+    return render_template("projects.html")
+
+@app.route('/latest/')
+def latest(): 
+    return render_template("latest.html")
+
+@app.route('/contact/')
+def contact(): 
+    return render_template("contact.html")
 
 if __name__ == "__main__": 
     app.run(debug=True)
